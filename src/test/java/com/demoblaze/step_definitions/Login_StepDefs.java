a7104c4168124cc127de1e9a3244293ba590717f
Mustafa Elmas <101817239+MustafaElmas2022@users.noreply.github.com>
2022-08-09 05:30:32 +0200

    Update Login_StepDefs.java

--- src/test/java/com/demoblaze/step_definitions/Login_StepDefs.java
+++ src/test/java/com/demoblaze/step_definitions/Login_StepDefs.java
@@ -1,12 +1,16 @@
 package com.demoblaze.step_definitions;
 
 import com.demoblaze.pages.LoginPage;
+import com.demoblaze.utulities.BrowserUtils;
 import com.demoblaze.utulities.ConfigurationReader;
 import com.demoblaze.utulities.Driver;
+import com.demoblaze.utulities.EncryptionUtils;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import static org.junit.Assert.*;
 
+import java.util.List;
 import java.util.Map;
 
 public class Login_StepDefs {
@@ -30,20 +34,38 @@ public class Login_StepDefs {
 
     @When("the user enters {string} and {string} and click login button")
     public void the_user_enters_and_and_click_login_button(String username, String password) {
-        loginPage.loginWithParameter_method(username,password);
+       loginPage.loginWithParameter_method(username,password);
     }
 
     @When("the user enters valid username and password")
     public void the_user_enters_valid_username_and_password(Map<String,String> userInfo) {
         loginPage.loginWithParameter_method(userInfo.get("username"),userInfo.get("password"));
 
-    }
 
+    }
 
     @Then("verify invalid credential {string}")
     public void verify_invalid_credential(String expectedMessage) {
 
         loginPage.verifyWithPopUpMessage_Method(expectedMessage);
+
+    }
+
+
+    @When("User enters encrypted credentials")
+    public void userEntersEncryptedCredentials() {
+
+        String username= EncryptionUtils.decrypt(ConfigurationReader.get("encryptedUsername"),System.getenv("secretKey"));
+        String password= EncryptionUtils.decrypt(ConfigurationReader.get("encryptedPassword"),System.getenv("secretKey"));
+        loginPage.loginWithParameter_method(username,password);
+
+
+    }
+
+    @Then("the user able to see following categories")
+    public void theUserAbleToSeeFollowingCategories(List<String> expectedList) {
+      List<String> actualList=  BrowserUtils.getElementsText(loginPage.categoriesList);
+       assertEquals("List does NOT MATCH",expectedList,actualList);
 
     }
 
